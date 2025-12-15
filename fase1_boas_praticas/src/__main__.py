76f3c5e8c9d05a22c2c7ba1c8cf3add176af6c3e
Leticia Faria <leticiaemanuella96@gmail.com>
2025-12-14 23:25:12 -0300

    boas praticas de estrutura de projetos

---
+++ fase1_boas_praticas/src/__main__.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from yaml import safe_load, loader # no terminal: uv add PyYaml
+from pathlib import Path
+
+def read_configs():
+    with open(Path(Path(__file__).parent, 'config/config.yaml'), 'r') as file:
+        config: Any = safe_load(file.read())
+
+    return config
+
+def main():
+    configs = read_configs()
+    print(configs['learning_rate'])
+
+def dummy_function():
+    dataframe: pd.DataFrame = pd.DataFrame({"A":[1, 2], "B":[3, 4]})
+    print(dataframe)
+
+if __name__ == "__main__":
+    main()
