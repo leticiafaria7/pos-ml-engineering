5fe30dbf705f7c83c7ab3f69bf5a16a756894fa7
Leticia Faria <leticiaemanuella96@gmail.com>
2026-03-08 21:41:35 -0300

    script para executar o scraping dos valores de cada acao a cada 1h

--- fase_2/etl_ibovespa/README.md
+++ fase_2/etl_ibovespa/README.md
@@ -4,7 +4,7 @@ A lista atualizada de ativos do Ibovespa (que no projeto foi acessada em 08/03/2
 
 A lista de todas as empresas listadas na B3 com seus respectivos setores está disponível [neste link](https://www.b3.com.br/pt_br/produtos-e-servicos/negociacao/renda-variavel/empresas-listadas.htm), opção "Busca por Setor de Atuação"
 
-As duas tabelas obtidas nos links acima são tratadas e relacionadas no notebook tratar_lista_empresas.ipynb
+As duas tabelas obtidas nos links acima são tratadas e relacionadas no notebook `tratar_lista_empresas.ipynb`
 
 Segue um resumo dos principais conceitos relacionados à Bolsa de Valores, importantes para o entendimento de negócio do projeto antes de seguir com a infraestrutura:
 
--- fase_2/etl_ibovespa/main.py
+++ fase_2/etl_ibovespa/main.py
@@ -1,51 +1,72 @@
+# --------------------------------------------------------------------------------------------------------------------- #
+# Imports
+# --------------------------------------------------------------------------------------------------------------------- #
+
 import pandas as pd
 import datetime
 import requests
-from bs4 import BeautifulSoup
 import time
+from tqdm import tqdm
+from bs4 import BeautifulSoup
 
-def save_stocks_values():
+# --------------------------------------------------------------------------------------------------------------------- #
+# Obter lista de ativos
+# --------------------------------------------------------------------------------------------------------------------- #
 
-    url = 'https://www.google.com/finance/markets/most-active'
+ativos_ibov = pd.read_excel('refined/ativos_ibov.xlsx')
+lista_ativos = list(ativos_ibov['ticker'].unique())
+
+# --------------------------------------------------------------------------------------------------------------------- #
+# Scraping
+# --------------------------------------------------------------------------------------------------------------------- #
+
+def get_stock_value(stock):
+    url = f'https://www.google.com/finance/quote/{stock}:BVMF?hl=pt'
 
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    items = soup.find('ul', {'class':'sbnBtf'}).find_all('div', {'class':'SxcTic'})
+    date = datetime.datetime.now()
+    price = soup.find('div', {'class':'YMlKec fxKbKc'}).text.encode('utf-8').decode('utf-8').replace('\xa0', ' ').strip()
+    # print(price)
 
-    df = pd.DataFrame()
+    df = pd.DataFrame(data = {'date':[date], 
+                              'codigo':[stock],
+                              'price':[price]})
+    
+    df['date'] = pd.to_datetime(df['date']).dt.round('min')
+    df['price'] = df['price'].apply(lambda x: x.replace('R$ ', '').replace(',', '.')).astype(float)
+    return df
 
-    for item in items:
-        date = datetime.datetime.now()
-        symbol = item.find('div', {'class':'iLEcy'}).find('div', {'class':'COaKTb'}).text
-        name = item.find('div', {'class':'ZvmM7'}).text
-        price = item.find('div', {'class':'YMlKec'}).text
-        change = item.find('span', {'class':'P2Luy'}).text
+# --------------------------------------------------------------------------------------------------------------------- #
+# Concatenar tabelas
+# --------------------------------------------------------------------------------------------------------------------- #
 
-        tmp = pd.DataFrame(
-            data = {
-                'date':[date],
-                'symbol':[symbol],
-                'name':[name],
-                'price':[price],
-                'change':[change]
-            }
-        )
+def get_table_stocks(lista_ativos):
 
-        df = pd.concat([df, tmp])
+    df_precos = pd.DataFrame()
 
-    df['date'] = pd.to_datetime(df['date'], format = '%Y-%m-%d %H:%M:%S')
-    # print(df.shape[0])
+    for ativo in tqdm(lista_ativos):
+        try:
+            tmp = get_stock_value(ativo)
+            df_precos = pd.concat([df_precos, tmp])
+        except Exception as e:
+            print(e)
+
+    df_precos = df_precos.reset_index(drop = True)
+
     filename = str(datetime.datetime.now())[:16].replace(' ', '-').replace(':', '-')
-    # print(filename)
+    df_precos.to_csv(f'scraped/file-{filename}.csv')
 
-    df.to_csv(f'data/file-{filename}.csv')
+# --------------------------------------------------------------------------------------------------------------------- #
+# Execução
+# --------------------------------------------------------------------------------------------------------------------- #
 
 if __name__ == '__main__':
 
     while True:
-        hora_exec = str(datetime.datetime.now())[:16]
-        save_stocks_values()
-        print(f"Salvando arquivo {hora_exec} | Horário atual: {str(datetime.datetime.now())[:16]}")
+        print(f"Início da execução: {str(datetime.datetime.now())[:16]}")
+        get_table_stocks(lista_ativos)
+        print(f"Salvando arquivo: {str(datetime.datetime.now())[:16]}")
         print()
         time.sleep(60*60)
     
